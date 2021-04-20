eeb56e1839206f38f437dd171d28adf85b89fa3e
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2021-04-20 12:56:05 -0400

    Update test-dca.R

--- tests/testthat/test-dca.R
+++ tests/testthat/test-dca.R
@@ -1,3 +1,11 @@
 test_that("dca() works", {
-  dca(cancer ~ cancerpredmarker, data = df_binary)
+  expect_error(
+    dca_binary <- dca(cancer ~ cancerpredmarker, data = df_binary),
+    NA
+  )
+
+  expect_error(
+    dca_surv <- dca(Surv(ttcancer, cancer) ~ cancerpredmarker, data = df_surv, time = 1),
+    NA
+  )
 })
